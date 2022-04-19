3afafb2c75e9aec9f1a87c08bef4e3ed4d8a8ca5
anupreetttt <anupreetpaulkar@gmail.com>
2022-04-18 20:44:15 -0500

    Added UI

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ function getrandom(){
     var randomNum1 = Math.floor(Math.random() * 6)+ 1;
     var randomNum2 = Math.floor(Math.random() * 6)+ 1;
 
-    document.getElementById('result').innerHTML = '<img src="dice' + randomNum1 + '.png">';
-    document.getElementById('result').innerHTML = '<img src="dice' + randomNum2 + '.png">';
+    var randomImage1 = "images/dice" + randomNum1 +".png";
+    var randomImage2 = "images/dice" + randomNum2 +".png";
 
-document.querySelector('h1').innerHTML = if (randomNum1 > randomNum2) {
-    
+   var image =  document.querySelectorAll("img")[0];
+   image.setAttribute("src", randomImage1);
+
+   var image2 =  document.querySelectorAll("img")[1];
+   image2.setAttribute("src", randomImage2);
+
+   if (randomNum1 > randomNum2) {
+       document.querySelector("h1").innerHTML = "🏆Player 1 won!";
+   } else if (randomNum2 > randomNum1)  {
+    document.querySelector("h1").innerHTML = "🏆Player 2 won!";
+   } else {
+    document.querySelector("h1").innerHTML = "🏁It's a tie!";
+   }
     
 }
 
-}
+getrandom();
+
