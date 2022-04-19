9090a73a7743d10d7b225803dae7554ab23e8e41
anupreetttt <anupreetpaulkar@gmail.com>
2022-04-18 19:36:46 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function getrandom(){
+    var randomNum1 = Math.floor(Math.random() * 6)+ 1;
+    var randomNum2 = Math.floor(Math.random() * 6)+ 1;
+
+    document.getElementById('result').innerHTML = '<img src="dice' + randomNum1 + '.png">';
+    document.getElementById('result').innerHTML = '<img src="dice' + randomNum2 + '.png">';
+
+document.querySelector('h1').innerHTML = if (randomNum1 > randomNum2) {
+    
+    
+}
+
+}
